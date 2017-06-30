faf6d87b252c8ca928b791f9260100cf8ba5391c
AlliBharath <allibharath123@gmail.com>
2017-06-30 16:30:03 -0500

    first commit

---
+++ Photos.php
@@ -0,0 +1,8 @@
+<?php
+$title = "Photos";
+
+
+
+include 'Template.php';
+?>
+
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/firefly1.jpg" class="imgLeft" />
+<h2>The Train Job</h2>
+<h3>Season 1 | Episode 1</h3>
+<p>
+   Six years later, Mal is the captain of his own transport ship, an older-model Firefly-class
+   vessel he named Serenity, and Zoe is his second-in-command. The rest of the ship crew 
+   consists of Wash (Alan Tudyk), who is the pilot and is also Zoes husband, Kaylee 
+   (Jewel Staite) the engineer, and Jayne Cobb (Adam Baldwin) the gun-for-hire. Inara 
+   (Morena Baccarin), a "Companion" (i.e. Courtesan) who rents one of Serenitys
+   two shuttles, normally travels with them, but she is currently away from the ship 
+   on business.While the crew are illegally salvaging some crates off an abandoned spaceship, they are discovered by an Alliance cruiser. To escape capture, they deploy a decoy distress beacon. The Alliance cruiser falls for the diversion, but broadcasts a bulletin that a Firefly-class ship is carrying stolen Alliance goods.
+   The crew of Serenity travel to Persephone, where they intend to deliver the stolen goods 
+   to Badger (Mark Sheppard), the small-time gang leader who hired them for the heist. When 
+   they meet with Badger, he reneges on their deal, partly because he is worried about the 
+   Alliance broadcast on the rogue Firefly, and partly because he doesnt like the way Mal 
+   looks down on him. Mal decides to try selling the cargo to Patience, an old business 
+   associate who lives on Whitefall.
+</p>
+
+<img src="Images/firefly2.jpg" class="imgRight"/>
+<h2>Bushwhacked</h2>
+<h3>Season 1 | Episode 2</h3>
+<p>
+   Serenity encounters a drifting space craft which turns out to be a short-range scow converted 
+   to a settler transport to the "Outer Planets". Mal decides to check out the derelict in order 
+   to either help survivors or loot the dead. He sends his crew off in teams to explore the ship, 
+   while he and Zoe head to a section that he suspects holds the more valuable supplies.They arrive 
+   in a room, followed shortly by River who points out mutilated passengers hanging from the ceiling. 
+   Mal knows what did this and orders everyone to regroup in the engine room, but en route Jayne is 
+   hit from behind by an unseen attacker, whom Jayne shoots blindly 4 times. Mal finds the wounded 
+   man hiding behind an air grate and they bring him back to the ship.As Simon treats the wounded man, 
+   Mal announces that he must be the lone survivor of a Reaver attack. He explains how the Reavers 
+   were driven insane after seeing the nothing at the "edge of the galaxy" and now commit unspeakable 
+   acts of evil. He allows Shepherd Book and Simon to cut down the victims and give them a modest 
+   ceremony, keeping them out of the way while Kaylee removes a Reaver booby trap that attached itself 
+   to Serenity when they docked earlier.Once the derelicts cargo is aboard, Serenity moves to leave, 
+   only to be stopped by an Alliance cruiser. The Feds board the ship and find Mal and everyone but River 
+   and Simon innocently awaiting them, the retrieved cargo plainly in sight to avoid accusations of theft. 
+   
+</p>
+
+<img src="Images/firefly3.png" class="imgLeft" />
+<h2>Our Mrs. Reynolds</h2>
+<h3>Season 1 | Episode 3</h3>
+<p>
+    A covered wagon makes its way through a shallow river. When a band of men on horseback 
+    stops it to demand its cargo, the driver Jayne and his "wife" Mal pull their weapons on 
+    the bandits and quickly defeat them with the help of Zoe. At a celebration party that 
+    night, Mal dances with a beautiful young woman, who gives him a wreath and wine, as an 
+    old man gives Jayne a rain stick made from wood.After Serenity is underway again, Mal 
+    encounters the young woman in the cargo bay. She informs him that she is now his wife. 
+    Zoe calls the entire crew down to enjoy the moment. Shepherd Book reads up on the local 
+    customs and informs Mal that he had taken part in a marriage ritual by accepting a wreath 
+    of flowers, drinking of her wine, and dancing together. Its "the marriage ceremony of the ".
+    "Triumph settlers." Mal has a heart-to-heart talk with the woman, who identifies herself as 
+    Saffron. Despite Mals encouragement to be her own woman, Saffron cooks him a meal as Zoe 
+    looks on in mocking disbelief. After eating, Mal escapes an offer to have his feet washed 
+    and visits Inara, who also shows contempt for Mals treatment of the situation. Jayne attempts 
+    to trade his favorite gun, named Vera, for Saffron, but Mal turns him down.Zoe and Wash 
+    continue to argue about Saffron. When Mal enters his quarters later, he is seduced by Saffron, 
+    whose lipstick contains a narcotic. 
+</p>';
+
+include 'Template.php';
+?>
+
